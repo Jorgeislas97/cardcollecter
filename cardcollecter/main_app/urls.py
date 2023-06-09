df3a4919de9877ad7410fcdd7c45eafb0678f5c6
Jorge Islas <Jorgeislas97@gmail.com>
2023-06-09 16:27:24 -0400

    pt2 complete

--- cardcollecter/main_app/urls.py
+++ cardcollecter/main_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.home, name='home'),
+    path('', views.home, name='home'),
+    path('about/',views.about, name='about'),\
+    path('cards/',views.cards_index, name='index'),
 
 ]
--- cardcollecter/main_app/views.py
+++ cardcollecter/main_app/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
+# views.py
+
+# Add this cats list below the imports
+cards = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cards_index(request):
+    return render(request, 'cards/index.html', {
+    'cards': cards
+  })
