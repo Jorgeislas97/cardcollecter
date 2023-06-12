440f7ab97b4d5a63d8e8ce0cfbfdd89859782d56
Jorge Islas <Jorgeislas97@gmail.com>
2023-06-12 15:15:55 -0400

    complete pt 3

--- cardcollecter/main_app/urls.py
+++ cardcollecter/main_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/',views.about, name='about'),\
     path('cards/',views.cards_index, name='index'),
-    path('cards/<int:card_id/>'), views.cards_detail, name='detail',
+    path('cards/<int:card_id>/', views.cards_detail, name='detail'),
 
 ]
