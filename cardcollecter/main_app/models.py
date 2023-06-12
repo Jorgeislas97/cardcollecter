5ef60530cd20f98692a6f86013bbf9d7f17cb1e0
Jorge Islas <Jorgeislas97@gmail.com>
2023-06-12 14:48:24 -0400

    saftey commit

--- cardcollecter/main_app/models.py
+++ cardcollecter/main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Card(models.Model):
+    name = models.CharField(max_length=100)
+    series = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    grade = models.IntegerField()
+
+
+# Changing this instance Method does not impact the database
+# therefore no makemigrations is needed 
+
+
+def __str__(self):
+    return self.name
--- cardcollecter/main_app/views.py
+++ cardcollecter/main_app/views.py
@@ -1,12 +1,7 @@
+# main_app/views.py
 from django.shortcuts import render
-
-# views.py
-
-# Add this cats list below the imports
-cards = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+# Import the Cat Model
+from .models import Card
 
 
 # Create your views here.
@@ -17,6 +12,7 @@ def about(request):
     return render(request, 'about.html')
 
 def cards_index(request):
+    cards = Card.objects.all()
     return render(request, 'cards/index.html', {
     'cards': cards
   })
